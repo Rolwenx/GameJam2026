9fd27267483e4c56d7ebf27ff2e79bad1d8d699e
Rolwen <n.rnolwen@gmail.com>
2026-02-07 02:33:13 +0100

    user can aim with mouse

---
+++ Assets/Scripts/PlayerAim.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class PlayerAim : MonoBehaviour
+{
+    private Transform aimTransform;
+
+    private void Awake()
+    {
+        aimTransform = transform.Find("Aim");
+    }
+
+
+    private void Update()
+    {
+        HandleAiming();
+    }
+
+    private void HandleAiming()
+    {
+        Vector3 mousePos = GetMouseWorldPosition();
+        Vector3 aimDirection = (mousePos - transform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        aimTransform.eulerAngles = new Vector3(0,0,angle);
+    }
+
+
+
+    // Get Mouse Position in World with Z = 0f [BY CODEMONKEY]
+    public static Vector3 GetMouseWorldPosition() {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
+    }
+
+    public static Vector3 GetMouseWorldPositionWithZ() {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+    }
+
+    public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera) {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
+    }
+
+    public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera) {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
+    }
+}
