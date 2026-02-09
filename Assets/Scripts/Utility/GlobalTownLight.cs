4bbf083b8b33bd6a4fb30e84d55f152860458a7e
Rolwen <n.rnolwen@gmail.com>
2026-02-09 12:50:02 +0100

    Light in level chooser depends on state of game completed

---
+++ Assets/Scripts/Utility/GlobalTownLight.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class GlobalTownLight : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Light2D playerLight;
+
+    private void Start()
+    {
+        ApplyLightProgression();
+    }
+
+    private void ApplyLightProgression()
+    {
+        float radius = playerLight.pointLightOuterRadius;
+
+        if (PlayerPrefs.GetInt("Level_1_Completed", 0) == 1)
+            radius = 10f;
+        else if (PlayerPrefs.GetInt("Level_2_Completed", 0) == 1)
+            radius = 15f;
+        else if (PlayerPrefs.GetInt("Level_3_Completed", 0) == 1)
+            radius = 20f;
+        else if (PlayerPrefs.GetInt("Level_4_Completed", 0) == 1)
+            radius = 50f;
+        else
+            radius = 5f;
+
+        playerLight.pointLightOuterRadius = radius;
+    }
+}
