9a7d1f6aaf60a4834f4ffaea82393c366bc99b35
Rolwen <n.rnolwen@gmail.com>
2026-02-09 21:26:36 +0100

    Updated SceneTransition.cs to save the active scene as last scene

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -5,15 +5,21 @@ public class SceneTransition : MonoBehaviour
 {
     public void GoToTown()
     {
+        PlayerPrefs.SetString("LastScene", SceneManager.GetActiveScene().name);
+        PlayerPrefs.Save();
         SceneManager.LoadScene("Town");    
     }
      public void GoToSceneChooser()
     {
+        PlayerPrefs.SetString("LastScene", SceneManager.GetActiveScene().name);
+        PlayerPrefs.Save();
         SceneManager.LoadScene("Pick Level");    
     }
 
     public void GoToGenerator()
     {
+        PlayerPrefs.SetString("LastScene", SceneManager.GetActiveScene().name);
+        PlayerPrefs.Save();
         SceneManager.LoadScene("Generator");    
     }
 }
