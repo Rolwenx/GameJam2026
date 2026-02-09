fc47006da399d6618786a1a239224d8339aa2e69
Roxane Braconnier <101722065+R0xane@users.noreply.github.com>
2026-02-09 18:15:35 +0100

    Adding voicelines + playerplef for generator end

--- Assets/Scripts/Generator/FinalGenerator.cs
+++ Assets/Scripts/Generator/FinalGenerator.cs
@@ -56,6 +56,8 @@ namespace Cainos.PixelArtTopDown_Basic
                 PlayerPrefs.GetInt("LV3Valide", 0) == 1 &&
                 PlayerPrefs.GetInt("LV4Valide", 0) == 1;
 
+            
+            PlayerPrefs.SetInt("AllLevelsCompleted", allValid ? 1 : 0);
             float targetAlpha = (isInside && allValid) ? 1f : 0f;
 
             for (int i = 0; i < runes.Count; i++)
