1b1a6bb3008d9f6afc9ccaec1d082141b7907c90
Rolwen <n.rnolwen@gmail.com>
2026-02-10 17:47:44 +0100

    Mid cinematique fin

---
+++ Assets/Scripts/Utility/LightFadeIn.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class SimpleLightFadeIn : MonoBehaviour
+{
+    public float fadeSpeed = 2f;
+    public float maxIntensity = 5f;
+
+    private Light myLight;
+
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+        myLight.intensity = 0f;
+    }
+
+    void Update()
+    {
+        if (myLight.intensity < maxIntensity)
+        {
+            myLight.intensity += fadeSpeed * Time.deltaTime;
+        }
+    }
+}
