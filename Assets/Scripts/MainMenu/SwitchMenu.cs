ec3161ca2012d38a9384bead974cfec2effa8abb
Roxane Braconnier <101722065+R0xane@users.noreply.github.com>
2026-02-10 01:58:01 +0100

    done for the sound lvl in MainMenu

--- Assets/Scripts/MainMenu/SwitchMenu.cs
+++ Assets/Scripts/MainMenu/SwitchMenu.cs
@@ -1,15 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SwitchToParam : MonoBehaviour
 {
     public GameObject menuPrincipal;
     public GameObject menuParametres;
     public GameObject menuElementsDébloqués;
+
+    public Slider sliderVolume;
+    public AudioSource musicAudioSource;
+    public TMP_Text volumeValueText; // Texte pour afficher la valeur du volume
     void Start()
     {
         menuPrincipal.SetActive(true);
         menuParametres.SetActive(false);
         menuElementsDébloqués.SetActive(false);
+        sliderVolume.value = musicAudioSource.volume; // initialiser le slider à la valeur actuelle du volume
+        volumeValueText.text = (musicAudioSource.volume * 100).ToString("0"); // afficher la valeur en pourcentage
     }
     
      public void GoToParam()
@@ -35,5 +43,18 @@ public class SwitchToParam : MonoBehaviour
         menuPrincipal.SetActive(false);
         menuElementsDébloqués.SetActive(true);
     }
+
+
+    
+
+    public void ChangeVolume()
+    {
+        if (musicAudioSource != null && sliderVolume != null)
+            musicAudioSource.volume = sliderVolume.value; // slider en 0..1
+            volumeValueText.text = (musicAudioSource.volume * 100).ToString("0"); // afficher la valeur en pourcentage
+    }
+
+
+    
 
 }
