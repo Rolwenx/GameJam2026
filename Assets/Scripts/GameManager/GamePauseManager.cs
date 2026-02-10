25d9c8c92052d48fab3f1b802e50216eae133233
Roxane Braconnier <101722065+R0xane@users.noreply.github.com>
2026-02-10 03:47:01 +0100

    pauseLogicFInishfor level 1

--- Assets/Scripts/GameManager/GamePauseManager.cs
+++ Assets/Scripts/GameManager/GamePauseManager.cs
@@ -6,6 +6,11 @@ public class GamePauseManager : MonoBehaviour
     public GameObject PauseMenu;
     public DialogueLevels dialogueManager; // Référence au DialogueManager
     public DialogueTutorialManager dialogueTutorialManager; // Référence au DialogueTutorialManager
+    public GameObject ParametersMenu; // Référence au menu de paramètres
+
+    // list des script à désactiver lors de la pause (ex: les scripts de dialogue pour éviter les dialogues qui continuent pendant la pause : souris)
+    public PlayerMovement playerMovementScript;
+    public PlayerAim playerAimScript;
 
     void Awake()
     {
@@ -14,54 +19,68 @@ public class GamePauseManager : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (!Input.GetKeyDown(KeyCode.Escape)) return;
+
+        // 1) Si les paramètres sont ouverts -> on ferme tout
+        if (ParametersMenu != null && ParametersMenu.activeSelf)
         {
-            if (PauseMenu.activeSelf)
-            {
-                Resume();
-            }
-            else
-            {
-                Pause();
-            }
+            Resume();
+            return;
+        }
+
+        // 2) Sinon toggle du menu pause
+        if (PauseMenu.activeSelf) {
+            Resume();
+
+        }
+        else {
+            Pause();
         }
     }
 
     public void Resume()
     {
         PauseMenu.SetActive(false);
-        Time.timeScale = 1f;
+        ParametersMenu.SetActive(false);
         
         // Réactiver le DialogueManager
         if (dialogueManager != null)
         {
             dialogueManager.enabled = true;
+            playerMovementScript.enabled = true;
+            playerAimScript.enabled = true;
         }
         if (dialogueTutorialManager != null)
         {
             dialogueTutorialManager.enabled = true;
+            playerMovementScript.enabled = true;
+            playerAimScript.enabled = true;
         }
+
+        
     }
 
     public void Pause()
     {
-        PauseMenu.SetActive(true);
-        Time.timeScale = 0f;
-        
+        PauseMenu.SetActive(true);        
         // Désactiver le DialogueManager
         if (dialogueManager != null)
         {
             dialogueManager.enabled = false;
+            playerMovementScript.enabled = false;
+            playerAimScript.enabled = false;
         }
         if (dialogueTutorialManager != null)
         {
             dialogueTutorialManager.enabled = false;
+            playerMovementScript.enabled = false;
+            playerAimScript.enabled = false;
         }
-    }
+
+        }
 
     public void QuitGame()
     {
-        Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenu");
     }
 }
