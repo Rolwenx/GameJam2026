56abf6bcf24a142893a72bf912f8af8b01deb41e
Roxane Braconnier <101722065+R0xane@users.noreply.github.com>
2026-02-10 19:54:12 +0100

    ambiant sound connected through the scenes

--- Assets/Scripts/GameManager/GestionVolume.cs
+++ Assets/Scripts/GameManager/GestionVolume.cs
@@ -3,6 +3,8 @@ using UnityEngine.UI;
 using TMPro;
 public class GestionVolume : MonoBehaviour
 {
+
+    private const string PREF_VOLUME = "MusicVolume";
     public Slider sliderVolume;
     public AudioSource musicAudioSource;
     public TMP_Text volumeValueText; // Texte pour afficher la valeur du volume
@@ -10,8 +12,12 @@ public class GestionVolume : MonoBehaviour
 
     void Start()
     {
-        sliderVolume.value = musicAudioSource.volume; // initialiser le slider à la valeur actuelle du volume
-        volumeValueText.text = effect +(musicAudioSource.volume * 100).ToString("0"); // afficher la valeur en pourcentage
+        if (!PlayerPrefs.HasKey(PREF_VOLUME)){
+            PlayerPrefs.SetFloat(PREF_VOLUME, 1f); // volume par défaut à 100%
+            PlayerPrefs.Save();
+        }
+        sliderVolume.value = PlayerPrefs.GetFloat(PREF_VOLUME); // initialiser le slider à la valeur actuelle du volume
+        volumeValueText.text = effect +(sliderVolume.value * 100).ToString("0"); // afficher la valeur en pourcentage
     }
     
 
@@ -20,5 +26,7 @@ public class GestionVolume : MonoBehaviour
         if (musicAudioSource != null && sliderVolume != null)
             musicAudioSource.volume = sliderVolume.value; // slider en 0..1
             volumeValueText.text = effect +(musicAudioSource.volume * 100).ToString("0"); // afficher la valeur en pourcentage
+        PlayerPrefs.SetFloat(PREF_VOLUME, sliderVolume.value); // sauvegarder la valeur du volume
+        PlayerPrefs.Save();
     }
 }
